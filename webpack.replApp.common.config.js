e2cca28783e1a0b1c668283f80e7361fdb219c5b
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-04-19 14:04:38 +0200

    concurrently & replApp

---
+++ webpack.replApp.common.config.js
@@ -0,0 +1,44 @@
+const InjectPlugin = require("webpack-inject-plugin")
+const path = require("path")
+
+module.exports = () => {
+    return {
+        entry: './replApp/index.ts',
+        output: {
+            filename: 'public/dist/app/replApp.js',
+            chunkFilename: 'public/dist/app/replApp.[name].js',
+            path: path.resolve(path.dirname('')),
+            publicPath: "/"
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: "tsconfig.replApp.json"
+                        }
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: ['to-string-loader', 'css-loader'],
+                },
+                {
+                    
+                    test: /\.(png|jpg|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+                    loader: 'url-loader?limit=100000'
+                },
+                {
+                    test: /\.pug$/,
+                    loader: ['raw-loader', 'pug-html-loader']
+                }
+            ]
+        }
+    }
+};
