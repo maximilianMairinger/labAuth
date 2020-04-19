9df4bf1c5c16ff40da31111b1a544763f247eb72
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-04-19 21:19:09 +0200

    del dist

--- replServer/dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("qwe");
-const xrray_1 = require("xrray");
-xrray_1.default(Array);
-//# sourceMappingURL=server.js.map
