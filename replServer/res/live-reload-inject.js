f6ab2eed128daed62cd9a2fcb297e2b3cfe980be
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-04-20 14:42:58 +0200

    Unregister sw on poked reload

--- replServer/res/live-reload-inject.js
+++ replServer/res/live-reload-inject.js
@@ -1,9 +1,21 @@
 let stream = new EventSource(url);
 
 
-stream.addEventListener("message", (event) => {
-  if (JSON.parse(event.data) === "reloadPlease") location.reload()
-})
+stream.addEventListener("message", reload)
+stream.addEventListener("error", reload)
 
-stream.addEventListener("error", location.reload.bind(location))
 
+async function reload() {
+  console.log("Reloading...")
+  let proms = []
+  await navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    console.log("Unregistering the following sw", registrations)
+    for(let registration of registrations) {
+      proms.push(registration.unregister())
+    }
+  })
+
+  await Promise.all(proms)
+
+  location.reload()
+}
