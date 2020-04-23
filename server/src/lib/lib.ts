54c3939fa7f780e976d0f53dbe1182b8a443a32c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-04-23 08:39:38 +0200

    [server]: architecture & simple calles

--- server/src/lib/lib.ts
+++ server/src/lib/lib.ts
@@ -1,4 +1,6 @@
-import crypto from "crypto"
+import * as crypto from "crypto"
+import * as tgm from "./../authTgm/authTgm"
+import delay from "delay"
 
 
 import * as MongoDB from 'mongodb'
@@ -11,25 +13,86 @@ const dbName = 'labAuth';
 
 
 
-export default async function init() {
-  let client = await MongoClient.connect(url, { useUnifiedTopology: true });
+export default async function init(salt: string) {
+  let client: MongoDB.MongoClient
+  
+  while (true) {
+    try {
+      client = await MongoClient.connect(url, { useUnifiedTopology: true });
+      break;
+    }
+    catch(e) {
+      console.error("Failed to connect to DB, retrying in 5sec")
+      await delay(5000)
+    }
+  }
+  
 
   const db = client.db(dbName);
 
   const sessionCollection = db.collection('session');
+  const cardCollection = db.collection('card');
+
+  sessionCollection.deleteMany({})
 
 
   return {
     async createTeacherSession(teacherUsername: string) {
       let sha = crypto.createHash('sha512');
-      sha.update(Math.random().toString() + "10ß923ioqwenjadfsavdsölnuhgzxyftdgzftxdft76");
-      let key = sha.digest("base64").toUpperCase();
+      sha.update(Math.random().toString() + salt);
+      let key = sha.digest("base64")
     
       await sessionCollection.insertOne({teacherUsername: teacherUsername, sessKey: key})
 
     
       
       return key;
-    }
+    },
+
+    getCardData(hashedCardId: string) {
+      return cardCollection.findOne({id: hashedCardId})
+    },
+
+    async startUnit(sessKey: string, metaData: any) {
+      metaData.start = new Date()
+      delay(metaData.hours * 60 * 60 * 1000).then(() => {
+        this.destroySession(sessKey)
+      })
+      await sessionCollection.findOneAndUpdate({sessKey}, {sessKey, metaData})
+    },
+
+    async getUnitData(sessKey: string) {
+      let e = await sessionCollection.findOne({sessKey})
+      delete e.sessKey
+      return e
+    },
+
+    async getLdapAuthData(username: string, password: string) {
+      try {
+        return await tgm.auth(username, password)
+      }
+      catch(e) {
+        return null
+      }
+    },
+
+    async saveCardData(personData: any, cardId: string) {
+      await cardCollection.insertOne({cardId, personData})
+    },
+    
+    async destroySession(sessKey: string) {
+      await sessionCollection.updateOne({sessKey}, { $unset: {sessKey} })
+    },
+
+    isStudent(data: any) {
+      if (!data) return null
+      return data.type === "student"
+    },
+
+    async registerStudent(studentData: any, lessonData: any) {
+      // await cardCollection.insertOne(data)
+    },
   }
+
+  
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,16 @@
-const args = require("yargs").argv
-const port = args.port === undefined ? console.log("Serving on port 443") as undefined || 443 : args.port;
-
+import * as generateSalt from "crypto-random-string"
 import * as express from "express"
 import * as bodyParser from "body-parser"
 import xrray from "xrray"; xrray(Array);
 import initLib from "./lib/lib"
+
+const args = require("yargs").argv
+const port = args.port === undefined ? console.log("Serving on port 443") as undefined || 443 : args.port;
+const salt = args.salt === undefined ? generateSalt({length: 15}) : args.salt
+const securityLevel: "paranoid" | "casual" = ["paranoid", "casual"].includes(args.securityLevel) ? "casual" : args.securityLevel
+const session = args.authKeyForRegistration === undefined ? console.warn("Unable to find \"authKeyForRegistration\" as command line option") as undefined || undefined : args.authKeyForRegistration
+
+
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,13 +20,70 @@ app.listen(port)
 app.use(express.static("./public"))
 
 
-initLib().then((lib) => {
-  
 
-  app.post("auth", () => {
-    lib.createTeacherSession("test123")
+initLib(salt).then((lib) => {
+
+
+
+  app.post("/startUnit", async (req, res) => {
+    await lib.startUnit(req.body.sessKey, req.body)
+
+    res.send(true)
   })
 
+  app.post("/destroySession", async (req, res) => {
+    await lib.destroySession(req.body.sessKey)
+    res.send(true)
+  })
+
+  app.post("/LDAPAuth", async (req, res) => {
+    let studentOrTeacherData = await lib.getLdapAuthData(req.body.username, req.body.password)
+
+    let isStudent = lib.isStudent(studentOrTeacherData)
+
+    if (isStudent) {
+      let studentData = studentOrTeacherData
+
+      await lib.saveCardData(studentData, req.body.hashedCardId)
+      let unitData = await lib.getUnitData(req.body.sessKey)
+      let data = await lib.registerStudent(studentData, unitData)
+      res.send({valid: true, data})
+
+    }
+    else if (isStudent === null) {
+      res.send({valid: false})
+    }
+    else {
+      let teacherDatas = studentOrTeacherData
+
+
+    }
+    
+    
+    
+  })
+
+  app.post("/studentSignOut", async (req, res) => {
+    
+  })
+
+  app.post("/cardAuth", async (req, res) => {
+    let cardData = await lib.getCardData(req.body.hashedCardId)
+    if (cardData !== null) res.send({entry: true, data: cardData})
+    else res.send({entry: false})
+  })
+
+  if (securityLevel === "casual") {
+    app.post("/cardIndex", async (req, res) => {
+
+    })
+  }
+
+  
+
+  
+
+  
 
   
 
