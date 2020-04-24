d225719c89590b16e965cc63a40dc99cd84aa474
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-04-24 12:37:12 +0200

    fixed dev env; rollup watches proeprly now

--- rollup.sw.dev.config.js
+++ rollup.sw.dev.config.js
@@ -4,7 +4,7 @@ import commonMod from "./rollup.server.common.config"
 
 export default merge(commonMod, {
   watch: {
-    include: 'serviceWorker',
+    include: 'serviceWorker/**',
     exclude: 'node_modules/**'
   }
 })
